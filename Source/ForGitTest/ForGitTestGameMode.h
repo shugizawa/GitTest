c8710ac24726c434abd643fe882f1de1a4a02b09
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-10-29 01:36:59 +0500

    Initial Push

---
+++ Source/ForGitTest/ForGitTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ForGitTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AForGitTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AForGitTestGameMode();
+};
+
+
+
